e6f29765a7e41b76462a09e15ad4dbbd8b8adbc5
jan_pfander <janlukas.pfaender@gmail.com>
2021-05-12 10:26:47 +0200

    update remove comments

--- data_frame_generator_across_subjects.py
+++ data_frame_generator_across_subjects.py
@@ -1,11 +1,6 @@
 import glob
 import pandas as pd
 
-# glob.glob('data*.csv') - returns List[str]
-# pd.read_csv(f) - returns pd.DataFrame()
-# for f in glob.glob() - returns a List[DataFrames]
-# pd.concat() - returns one pd.DataFrame()
 df = pd.concat([pd.read_csv(data, comment='#', na_values='None') for data in glob.glob('*.xpd')], ignore_index = True)
-
 
 df.to_csv("all_reaction_times.csv")
